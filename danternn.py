d971ebda33bef4835e4552461d0255f36e8196f9
Alessandro Pavesi <pavesialessandro@live.it>
2020-09-15 12:09:23 +0200

    New method for sampling

--- danternn.py
+++ danternn.py
@@ -209,6 +209,15 @@ def generate_text(model):
   return text_generated
 
 
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    # From https://github.com/llSourcell/keras_explained/blob/master/gentext.py
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
 
 generative_model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
 
